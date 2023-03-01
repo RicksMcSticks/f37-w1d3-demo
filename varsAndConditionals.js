da6d5574265ba4b20ddd72dce60955f02bcd5fdc
Cameron Ricks <cameron6x6@gmail.com>
2023-03-01 13:59:52 -0700

    initial commit

---
+++ varsAndConditionals.js
@@ -0,0 +1,15 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+let obiwanAttack = 25
+let anakinAttack = 35
+
+if (obiwanAttack > anakinAttack) {
+    console.log('Obiwan has a better attack than Anakin.')
+} else if (anakinAttack > obiwanAttack) {
+    console.log('Anakin has a better attack than Obiwan.')
+} else {
+    console.log('Anakin and Obiwan have the same attack.')
+}
